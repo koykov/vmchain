7edce7f7c977cdd38531ab6940d571823c129217
Konstantin Voykov <koykov0@gmail.com>
2026-02-01 23:13:41 +0300

    gauge: cover with bench and parallel bench

--- gauge_test.go
+++ gauge_test.go
@@ -1,46 +1,114 @@
 package vmchain
 
 import (
+	"math"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
+func gfn() GaugeChain {
+	return Gauge("myservice_feature", nil).
+		WithLabel("groupID", "foobar").
+		WithLabel("countryID", "123")
+}
+
 func TestGauge(t *testing.T) {
-	fn := func() GaugeChain {
-		return Gauge("myservice_feature", nil).
-			WithLabel("groupID", "foobar").
-			WithLabel("countryID", "123")
-	}
 	t.Run("add", func(t *testing.T) {
-		fn().Add(10)
-		fn().Add(10)
-		v := fn().Get()
+		gfn().Add(10)
+		gfn().Add(10)
+		v := gfn().Get()
 		assert.Equal(t, float64(20), v)
 	})
 	t.Run("set", func(t *testing.T) {
-		fn().Set(15)
-		v := fn().Get()
+		gfn().Set(15)
+		v := gfn().Get()
 		assert.Equal(t, float64(15), v)
 	})
 	t.Run("inc", func(t *testing.T) {
-		fn().Set(0)
-		fn().Inc()
-		fn().Inc()
-		fn().Inc()
-		fn().Inc()
-		fn().Inc()
-		v := fn().Get()
+		gfn().Set(0)
+		gfn().Inc()
+		gfn().Inc()
+		gfn().Inc()
+		gfn().Inc()
+		gfn().Inc()
+		v := gfn().Get()
 		assert.Equal(t, float64(5), v)
 	})
 	t.Run("dec", func(t *testing.T) {
-		fn().Set(10)
-		fn().Dec()
-		fn().Dec()
-		fn().Dec()
-		fn().Dec()
-		fn().Dec()
-		v := fn().Get()
+		gfn().Set(10)
+		gfn().Dec()
+		gfn().Dec()
+		gfn().Dec()
+		gfn().Dec()
+		gfn().Dec()
+		v := gfn().Get()
 		assert.Equal(t, float64(5), v)
+	})
+}
+
+func BenchmarkGauge(b *testing.B) {
+	b.Run("add", func(b *testing.B) {
+		b.ReportAllocs()
+		for i := 0; i < b.N; i++ {
+			gfn().Add(1)
+		}
+	})
+	b.Run("set", func(b *testing.B) {
+		b.ReportAllocs()
+		for i := 0; i < b.N; i++ {
+			gfn().Add(float64(i))
+		}
+	})
+	b.Run("inc", func(b *testing.B) {
+		b.ReportAllocs()
+		gfn().Set(0)
+		for i := 0; i < b.N; i++ {
+			gfn().Inc()
+		}
+	})
+	b.Run("dec", func(b *testing.B) {
+		b.ReportAllocs()
+		gfn().Set(math.MaxFloat64)
+		for i := 0; i < b.N; i++ {
+			gfn().Dec()
+		}
+	})
+}
+
+func BenchmarkGaugeParallel(b *testing.B) {
+	b.Run("add", func(b *testing.B) {
+		b.ReportAllocs()
+		b.RunParallel(func(pb *testing.PB) {
+			for pb.Next() {
+				gfn().Add(1)
+			}
+		})
+	})
+	b.Run("set", func(b *testing.B) {
+		b.ReportAllocs()
+		b.RunParallel(func(pb *testing.PB) {
+			for pb.Next() {
+				gfn().Set(123)
+			}
+		})
+	})
+	b.Run("inc", func(b *testing.B) {
+		b.ReportAllocs()
+		gfn().Set(0)
+		b.RunParallel(func(pb *testing.PB) {
+			for pb.Next() {
+				gfn().Inc()
+			}
+		})
+	})
+	b.Run("dec", func(b *testing.B) {
+		b.ReportAllocs()
+		gfn().Set(math.MaxFloat64)
+		b.RunParallel(func(pb *testing.PB) {
+			for pb.Next() {
+				gfn().Dec()
+			}
+		})
 	})
 }
